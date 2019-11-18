a38ebbf5f01fc3f74454ec3590e1c47cd81dc001
Ian Duckworth <ianleeduckworth@gmail.com>
2019-11-18 12:07:12 -0500

    home page restyle

--- src/pages/landing/home.tsx
+++ src/pages/landing/home.tsx
@@ -5,14 +5,14 @@ export const Home: React.FC<{}> = () => {
     <>
       <div className="container">
         <h1 className="py-3 text-center">Welcome to Hope's Recipes!</h1>
-        <h4 className="text-center font-italic">
+        <h5 className="text-center font-italic">
           This site houses all of Hope's recipes in one easy to find place. To
           find a recipe, simply click into the search bar above and type! Click
           "Search" (or press Enter) to see all matching recipes. Click the
           recipe card to see the recipe details including ingredients, steps,
           and cook time. It you want to see a list of all recipes, click Search
           without entering any text into the Search box.
-        </h4>
+        </h5>
       </div>
     </>
   );
