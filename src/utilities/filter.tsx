c1deaae9cd5cecab4e5df09c9510f8936dfe1493
Hope Duckworth <hope.duckworth@gmail.com>
2019-12-06 22:41:09 -0500

    added feature that sorts recipes alphabetically

--- src/utilities/filter.tsx
+++ src/utilities/filter.tsx
@@ -17,6 +17,19 @@ export const filterRecipes = (
   return unique([...titleClause, ...tagsClause]);
 };
 
+export const sortByTitleAlphabetical = (a: Recipe, b: Recipe) => {
+  const titleA = a.title.toUpperCase();
+  const titleB = b.title.toUpperCase();
+
+  let comparison = 0;
+  if (titleA > titleB) {
+    comparison = 1;
+  } else if (titleA < titleB) {
+    comparison = -1;
+  }
+  return comparison;
+};
+
 export const unique = (array: any[]) => {
   var a = array.concat();
   for (var i = 0; i < a.length; ++i) {
